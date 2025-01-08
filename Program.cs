4f6ec08214b492b0b0922b962de3a34ea1ce23cd
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-08 17:27:34 +0200

    iterating over an array using a for loop

--- Program.cs
+++ Program.cs
@@ -39,8 +39,29 @@ numberArray[4] = 5;
 // access and print the elemnet at index zero of the array.
 Console.WriteLine($"The element at index 0 is {numberArray[0]}"); 
 
+Console.Clear(); // clear the console
+
 string[] names = {"Lungile", "Adam", "Mpumie"}; // create and initialize the array with some values 
 
 Console.WriteLine($"There are {names.Length} people in the list.");
 Console.WriteLine($"The name of the programmer is {names[0]}");
 
+// reasign the elements in an array 
+
+
+names[1] = "Wisdom";
+names[2] = "Nompumelelo";
+
+Console.WriteLine($"The new names in the list are {names[0]}, {names[1]}, and {names[2]} ");
+
+// lets get the names in the lists and the qualifications of each person using parallel arrays 
+
+string[] qualification = new string[3];
+
+qualification[0] = "Full-Stack Developer";
+qualification[1] = "Programmer";
+qualification[2] = "Front-End Developer";
+
+for (int i = 0 ; i < qualification.Length ; i++){
+    Console.WriteLine($"{names[i]} is a certified {qualification[i]}");
+}
